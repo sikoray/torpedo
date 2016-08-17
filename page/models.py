cecd89267b073d825ab9eda108421ff3b4c1310c
sikoray <sikora.y@nton.info>
2016-08-18 02:29:50 +0300

    Changed the HTML for the site.

--- page/models.py
+++ page/models.py
@@ -6,16 +6,30 @@ from PIL import Image
 class Article(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    title1 = models.CharField(max_length=200)
     text1 = models.TextField()
     image1 = models.ImageField(blank=True)
+    price1 = models.CharField(max_length=200)
+    title2 = models.CharField(max_length=200)
     text2 = models.TextField()
     image2 = models.ImageField(blank=True)
+    price2 = models.CharField(max_length=200)
+    title3 = models.CharField(max_length=200)
     text3 = models.TextField()
     image3 = models.ImageField(blank=True)
+    price3 = models.CharField(max_length=200)
+    title4 = models.CharField(max_length=200)
     text4 = models.TextField()
     image4 = models.ImageField(blank=True)
+    price4 = models.CharField(max_length=200)
+    title5 = models.CharField(max_length=200)
     text5 = models.TextField()
     image5 = models.ImageField(blank=True)
+    price5 = models.CharField(max_length=200)
+    title6 = models.CharField(max_length=200)
+    text6 = models.TextField()
+    image6 = models.ImageField(blank=True)
+    price6 = models.CharField(max_length=200)
 
 
     created_date = models.DateTimeField(
@@ -34,6 +48,11 @@ class Trademark(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     tm1 = models.ImageField(blank=True)
+    tm2 = models.ImageField(blank=True)
+    tm3 = models.ImageField(blank=True)
+    tm4 = models.ImageField(blank=True)
+    tm5 = models.ImageField(blank=True)
+    tm6 = models.ImageField(blank=True)
     
     
     created_date = models.DateTimeField(
@@ -54,6 +73,11 @@ class Slide(models.Model):
     title = models.CharField(max_length=200)
     slidetext1 = models.TextField()
     slideimage1 = models.ImageField(blank=True)
+    slidetext2 = models.TextField()
+    slideimage2 = models.ImageField(blank=True)
+    slidetext3 = models.TextField()
+    slideimage3 = models.ImageField()
+    aboutustext = models.TextField()
     
 
     created_date = models.DateTimeField(
